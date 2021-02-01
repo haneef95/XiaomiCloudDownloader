e5d1097041e217132e3cdea52fd4f4420beb55eb
haneef95 <298208+haneef95@users.noreply.github.com>
2021-02-01 00:58:31 +0000

    Create miGalleryDownloader.js
    Working Image downloader

---
+++ miGalleryDownloader.js
@@ -0,0 +1,21 @@
+function randomInt(min, max){
+  const r = Math.random()*(max-min) + min  
+  return Math.floor(r)
+}
+
+function sleep(ms) {
+  console.log("Sleeping for: "+ ms)
+  return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+async function keepDownloadingImgs() {
+  while(1){
+    await sleep(randomInt(755,1100));
+    console.log("Attempting download...");
+    document.querySelector('.download-c_kuy').click();
+    await sleep(randomInt(1100,1900));
+    console.log("Attempting next image...");
+    document.querySelector('.ico-next-4KyWW').click()
+  }
+}
+keepDownloadingImgs();
